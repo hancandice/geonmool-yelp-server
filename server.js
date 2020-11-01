6f4a411060b84d528777b2406ed2f34faa7aaa24
Candice Han <jeeyoung.han82@gmail.com>
2020-11-01 23:35:46 +0900

    BASIC SETUP

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.get("/getRestaurants", (req, res) => {
+  res.status(404).json({
+    status: "success",
+    restaurant: "McDonalds",
+  });
+});
+//http://localhost:3006/getRestaurants
+//To Change the status code: res.status(status_code)
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`server is up and listing on port ${port}`);
+});
