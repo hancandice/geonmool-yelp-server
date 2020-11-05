24f87b4778863e772d75a32e0811102616297499
Candice Han <jeeyoung.han82@gmail.com>
2020-11-06 00:01:39 +0900

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -11,3 +11,22 @@ INSERT INTO reviews (name, review, rating) values ('한지영', '맛있었어용
 INSERT INTO reviews (restaurant_id, name, review, rating) values (18, 'mark', 'This restaurant sucked', 2);
 
 select * from reviews where restaurant_id = 1;
+
+-- =========== Postgres Aggregate Functions ===========
+
+select count (*) from reviews;
+
+select trunc(AVG(rating), 3) AS average_review from reviews;
+
+select name as username, rating as restaurant_rating from reviews;
+
+select AVG(rating) from reviews where restaurant_id = 1;
+
+select trunc(AVG(rating), 2) AS average_review from reviews where restaurant_id=3;
+
+select count(rating) from reviews where restaurant_id = 1;
+
+select location, count(location) from restaurants group by location;
+
+select restaurant_id, count(restaurant_id) from reviews group by restaurant_id;
+
